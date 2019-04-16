1a8994c5b2e64355f5346476c88334e90414a265
Blake Lockley <bwlockley@gmail.com>
2019-04-16 17:53:36 +1000

    Added json option

--- zipcode.py
+++ zipcode.py
@@ -27,12 +27,26 @@ def dense_map():
     return mapping
 
 
+def json_map():
+    import json
+
+    padded_map = {}
+    for (zipcode, state) in dense_map().items():
+        padded_zipcode = str(zipcode).rjust(5, '0')
+        padded_map[padded_zipcode] = state
+
+    return json.dumps(padded_map, indent=4)
+
 if __name__ == '__main__':
     import sys
     
     if len(sys.argv) != 2:
-        print(f"usage: python {sys.argv[0]} [-d | --dense | <zipcode>]")
+        print(f"usage: python {sys.argv[0]} [--json | --dense | <zipcode>]")
         exit(1)
+    
+    if sys.argv[1] in ["-j", "--json"]:
+        print(json_map())
+        exit(0) 
 
     if sys.argv[1] in ["-d", "--dense"]:
         print(dense_map())
